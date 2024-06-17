be41231db9ddeeafd35d9a91a5437443d57bcf98
sungwon <thesungwon@gmail.com>
2024-06-18 00:26:22 +0900

    Responses

--- src/server.js
+++ src/server.js
@@ -4,7 +4,16 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("home send");
+};
+const handleLogin = (req, res) => {
+  return res.send("login send");
+};
+
 const handleListening = () =>
   console.log(`server listening on port http://localhost:${PORT}`);
 
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 app.listen(4000, handleListening);
