62fa1b4a0357f5bbf504d5a0c9489a8c2f165fc8
sungwon <thesungwon@gmail.com>
2024-06-23 23:25:44 +0900

    4.1 making routers

---
+++ README.md
@@ -0,0 +1,24 @@
+#Wetube Reloaded
+
+#ROUTER
+
+\*Global Routers
+/ home
+/join 회원가입
+/login 로그인
+/search 검색
+
+---
+
+\*User Routers
+/users/edit
+/users/delete
+
+---
+
+\*Video Routers
+/videos/watch
+/videos/edit
+/videos/delete
+/videos/comment
+/videos/comments/delete
--- src/server.js
+++ src/server.js
@@ -5,40 +5,26 @@ import morgan from "morgan";
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
-
-// const logger = (req, res, next) => {
-//   console.log(`${req.method} ${req.url}`);
-//   next();
-// };
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  console.log(url.includes("protected"));
-  if (url === "/protected") {
-    return res.send("<h1> NOT ALLOWED </h1>");
-  }
-  console.log("Allowed, You May Continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("home send");
-};
-const handleLogin = (req, res) => {
-  return res.send("login send");
-};
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the Private Lounge");
-};
+app.use(logger);
 
 const handleListening = () =>
   console.log(`server listening on port http://localhost:${PORT}`);
 
-// app.get("/", middleware, handleHome);
+//
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
 
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
-app.listen(4000, handleListening);
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("edit user");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("watch video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+app.listen(PORT, handleListening);
