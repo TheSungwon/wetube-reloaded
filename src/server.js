0dd074b202cb4998831d6fa79379b5746eef6e1f
sungwon <thesungwon@gmail.com>
2024-06-23 23:43:11 +0900

    cleaning the CODE

--- src/server.js
+++ src/server.js
@@ -1,30 +1,19 @@
 import express from "express";
 // const express = require("express"); import와 같은 표현
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
 
-const handleListening = () =>
-  console.log(`server listening on port http://localhost:${PORT}`);
-
-//
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("edit user");
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("watch video");
-videoRouter.get("/watch", handleWatchVideo);
-
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
