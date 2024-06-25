8b5569f9acfd124c56821e85f5d7f4197871b195
sungwon <thesungwon@gmail.com>
2024-06-25 23:48:01 +0900

    partials PUG

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
 export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => res.send("edit video");
 export const deleteVideo = (req, res) => res.send("Delete video");
 export const upload = (req, res) => res.send("upload video");
