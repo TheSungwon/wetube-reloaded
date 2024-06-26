e7d866b14a7151216c8fd799785773bfcccc6895
sungwon <thesungwon@gmail.com>
2024-06-27 01:02:24 +0900

    PUG template's iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,8 +3,10 @@ const fakeUser = {
   loggedIn: false,
 };
 
+const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 export const trending = (req, res) =>
-  res.render("home", { pageTitle: "dddd", fakeUser });
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
