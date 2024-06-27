fd877d033c1c2cd2ef66f28ab6dbaafa4794f347
sungwon <thesungwon@gmail.com>
2024-06-28 01:12:11 +0900

    Array Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 const fakeUser = {
   username: "user5434",
-  loggedIn: false,
+  loggedIn: true,
 };
 
 const videos = [
@@ -17,7 +17,7 @@ const videos = [
     rating: 5,
     comments: 2,
     createAt: "2 minutes ago",
-    views: 345,
+    views: 1,
     id: 2,
   },
   {
@@ -32,8 +32,18 @@ const videos = [
 
 export const trending = (req, res) =>
   res.render("home", { pageTitle: "Home", fakeUser, videos });
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", {
+    pageTitle: `Watching ${video.title}`,
+    fakeUser,
+    video,
+  });
+};
+
+export const edit = (req, res) => res.render("edit", { fakeUser });
 export const search = (req, res) => res.send("search");
 export const deleteVideo = (req, res) => res.send("Delete video");
 export const upload = (req, res) => res.send("upload video");
