0ee30f810eec8917086e7306ea1f63e83cdbc3d6
sungwon <thesungwon@gmail.com>
2024-07-03 00:13:07 +0900

    Returns and Renders

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,7 +8,14 @@ const fakeUser = {
 //mongoose는 더 이상 callback함수를 지원하지 않음. promise로 불러와야함.
 //Video.find({}, (error, document) => {}); callback 사용불가
 
+//callback은 return이 되지 않음.
+//return은 필수가 아니지만 (function 안 에서) 필요한 function을 불러야함.
 //await을 사용하려면 function에 async 선언
+
+// 1. return의 역할 : 본질적인 return의 역할보다는 function을 마무리짓는 역할로 사용되고 있음.
+// - 이러한 경우 return이 없어도 정상적으로 동작하지만 실수를 방지하기 위해 return을 사용
+// 2. render한 것은 다시 render할 수 없음
+// - redirect(), sendStatus(), end() 등등 포함 (express에서 오류 발생)
 export const home = async (req, res) => {
   const videos = await Video.find({});
   console.log(videos);
