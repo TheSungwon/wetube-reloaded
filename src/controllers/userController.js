4060e1b702dab63d5bda3aea0871a24e3ee72842
sungwon <thesungwon@gmail.com>
2024-07-12 01:26:26 +0900

    Create Account Part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,13 @@
+import Users from "../models/User";
+
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "create Account" });
 
 export const postJoin = async (req, res) => {
   console.log(req.body);
-  return res.end();
+  const { name, email, username, password, location } = req.body;
+  await Users.create({ name, email, username, password, location });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("edit user");
 export const remove = (req, res) => res.send("remove user");
