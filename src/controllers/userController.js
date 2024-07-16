46332b86cdafde102914bb6c427145c0eb152157
sungwon <thesungwon@gmail.com>
2024-07-17 00:50:40 +0900

    Login Part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,12 +23,36 @@ export const postJoin = async (req, res) => {
       errorMessage: "This Username Or Email is Already Taken.",
     });
   } else {
-    await Users.create({ name, email, username, password, location });
-    return res.redirect("/login");
+    try {
+      await Users.create({ name, email, username, password, location });
+      return res.redirect("/login");
+    } catch (error) {
+      return res.status(400).render("join", {
+        pageTitle: "create a User Error",
+        errorMessage: error._message,
+      });
+    }
   }
 };
+export const getLogin = (req, res) => res.render("login");
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res
+      .status(404)
+      .render("login", {
+        pageTitle: "login error",
+        errorMessage: "An Account With This Username Dose Not Exists.",
+      });
+  }
+
+  return res.render("login");
+  // return res.end();
+};
+
 export const edit = (req, res) => res.send("edit user");
 export const remove = (req, res) => res.send("remove user");
-export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see user");
