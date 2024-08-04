07fefd9a6a0386c4061b8cfe26607484a2b201f1
sungwon <thesungwon@gmail.com>
2024-08-05 00:40:13 +0900

    Github Login part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -69,6 +69,24 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+
+  const config = {
+    client_id: "Ov23lin6qspDU9Ssqg73",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+
+  const params = new URLSearchParams(config).toString();
+
+  const finalUrl = `${baseUrl}?${params}`;
+
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
+
 export const edit = (req, res) => res.send("edit user");
 export const remove = (req, res) => res.send("remove user");
 export const logout = (req, res) => res.send("logout");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 const userRouter = express.Router();
 
 userRouter.get("/", (req, res) => res.send("users"));
@@ -7,5 +14,7 @@ userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 userRouter.get("/logout", logout);
 userRouter.get("/:id(\\d+)", see);
+userRouter.get("github/start", startGithubLogin);
+userRouter.get("github/start", finishGithubLogin);
 
 export default userRouter;
