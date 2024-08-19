790dcc6afa4a973eaa7895be4cc2d9f3268fa258
sungwon <thesungwon@gmail.com>
2024-08-20 01:22:54 +0900

    File Uploads part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -166,7 +166,7 @@ export const postEdit = async (req, res) => {
   //req.body 를 구조분해할당
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
@@ -196,6 +196,7 @@ export const postEdit = async (req, res) => {
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
