55c4d53511041f6a35b8c7c9080454e65ba101d2
sungwon <thesungwon@gmail.com>
2024-08-21 17:02:55 +0900

    Better Developer Experience

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ module.exports = {
     }),
   ],
   mode: "development",
+  watch: true, // npm run assets 자동실행
+  clean: true, // build 전 output folder를 clean
   output: {
     filename: "js/main.js",
     // path: "./assets/js",
